9baab8e710f0752de02fade2b0fbb3964926c421
April.Forones <aprilforones13@gmail.com>
2024-06-26 11:36:37 +0800

    Use map and Map Services Fix

--- src/services/mapServices.ts
+++ src/services/mapServices.ts
@@ -34,10 +34,27 @@ export async function getObjectById(id: string): Promise<ObjectItem> {
   }
 }
 
+// export async function getCategories(): Promise<Category[]> {
+//   try {
+//     const response = await apiService.get("/categories");
+//     return response.data as Category[];
+//   } catch (error) {
+//     console.error("Error fetching categories:", error);
+//     throw error;
+//   }
+// }
+
 export async function getCategories(): Promise<Category[]> {
   try {
-    const response = await apiService.get("/categories");
-    return response.data as Category[];
+    // const response = await apiService.get("/categories");
+    // return response.data as Category[];
+    const response =  await fetch("db.json");
+    const data = await response.json();
+
+    console.log(data.categor);
+
+    return data.Category;
+
   } catch (error) {
     console.error("Error fetching categories:", error);
     throw error;
@@ -48,8 +65,19 @@ export async function createCategory(
   newCategory: Partial<Category>
 ): Promise<Category | null> {
   try {
-    const response = await apiService.post("/categories", newCategory);
-    return response.data as Category;
+    // const response = await apiService.post("/categories", newCategory);
+    // return response.data as Category;
+
+    const response = await fetch("db.json");
+    const data = await response.json();
+    const categor = data.categor.find((categ: Category) => categ.id === newCategory.id);
+    console.log('Single object', categor);
+
+    if (!categor) {
+      throw new Error(`Category with ID ${newCategory} not found`);
+    }
+    return categor;
+
   } catch (error) {
     console.error("Error creating category:", error);
     throw error;
@@ -58,7 +86,18 @@ export async function createCategory(
 
 export async function deleteCategory(id: string): Promise<void> {
   try {
-    await apiService.delete(`/categories/${id}`);
+    // await apiService.delete(`/categories/${id}`);
+    const response = await fetch("db.json");
+    const data = await response.json();
+    const categor = data.categor.find((categ: Category) => categ.id === id);
+
+    console.log('Single categories', categor);
+
+    if (!categor) {
+      throw new Error(`categories with ID ${id} not found`);
+    }
+    return categor;
+
   } catch (error) {
     console.error(`Error deleting category with ID ${id}:`, error);
     throw error;
@@ -70,8 +109,20 @@ export async function updateObject(
   updatedData: Partial<ObjectItem>
 ): Promise<ObjectItem | null> {
   try {
-    const response = await apiService.put(`/objects/${id}`, updatedData);
-    return response.data as ObjectItem;
+    // const response = await apiService.put(`/objects/${id}`, updatedData);
+    // return response.data as ObjectItem;
+    const response = await fetch("db.json");
+    const data = await response.json();
+    //const object = data.objects.find((obj: ObjectItem) => updatedData);
+    const object = data.objects.find((obj: ObjectItem) => obj.id === updatedData);
+
+    console.log('Single object', object);
+
+    if (!object) {
+      throw new Error(`Object with ID ${id} not found`);
+    }
+    return object;
+
   } catch (error) {
     console.error(`Error updating object with ID ${id}:`, error);
     throw error;
@@ -83,8 +134,20 @@ export async function updateCategory(
   updatedData: Partial<Category>
 ): Promise<Category | null> {
   try {
-    const response = await apiService.put(`/categories/${id}`, updatedData);
-    return response.data as Category;
+    // const response = await apiService.put(`/categories/${id}`, updatedData);
+    // return response.data as Category;
+
+    const response = await fetch("db.json");
+    const data = await response.json();
+    const categor = data.categor.find((categ: Category) => categ.id === updatedData.id);
+
+    console.log('Single object', categor);
+
+    if (!categor) {
+      throw new Error(`Object with ID ${id} not found`);
+    }
+    return categor;
+
   } catch (error) {
     console.error(`Error updating category with ID ${id}:`, error);
     throw error;
