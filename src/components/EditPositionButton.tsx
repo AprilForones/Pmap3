c7ad84da3f189ed31539d82ef97401c0e4dd9502
April.Forones <aprilforones13@gmail.com>
2024-09-18 14:20:10 +0800

    1809

--- src/components/EditPositionButton.tsx
+++ src/components/EditPositionButton.tsx
@@ -2,21 +2,32 @@ import { FiMapPin } from "react-icons/fi";
 import Tooltip from "./ui/Tooltip";
 import { NavigationContext } from "@/pages/Map";
 import { NavigationContextType } from "@/utils/types";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { resetEdges } from "@/utils/navigationHelper";
 
 function EditPositionButton() {
-  const { isEditMode, setIsEditMode, setNavigation } = useContext(
+  // const { isEditMode, setIsEditMode, setNavigation } = useContext(
+  //   NavigationContext
+  const {setIsEditMode, setNavigation } = useContext(
     NavigationContext
   ) as NavigationContextType;
+
   function handleEdit() {
-    setIsEditMode(!isEditMode);
+   // setIsEditMode(!isEditMode);
+    setIsEditMode(true);
     resetEdges();
     setNavigation((prevNavigation) => ({
       ...prevNavigation,
       end: "",
     }));
   }
+
+// Automatically run the function when the component loads
+useEffect(() => {
+  handleEdit();  // This will trigger the circles to show when the page loads
+}, []); // Empty dependency array ensures it only runs once, when the component mounts
+
+
   return (
     <Tooltip content="Change Position" className="bg-green-500">
       <button
